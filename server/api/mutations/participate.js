3d9c3ed5993b9855bfdffa37cef2132b979c1889
Peter Alexander Garnæs <petergarnaes@gmail.com>
2017-04-20 12:34:56 +0200

    Added expecption, so will_be_late property can be changed after shopping has completed

--- server/api/mutations/participate.js
+++ server/api/mutations/participate.js
@@ -3,9 +3,9 @@
  */
 
 import {
-  GraphQLInputObjectType as InputObjectType,
-  GraphQLString as StringType,
-  GraphQLNonNull as NonNull,
+    GraphQLInputObjectType as InputObjectType,
+    GraphQLString as StringType,
+    GraphQLNonNull as NonNull,
 } from 'graphql';
 import {resolver,attributeFields} from 'graphql-sequelize';
 import UserParticipationType from '../types/UserParticipationType';
@@ -16,60 +16,64 @@ import {csrf_check,csrf_error_message} from '../csrf_check';
 // The sequelize validation will take care of input validation, and correctly reject wrong input.
 // Only authorization must be done.
 const participate = {
-  //type: UserParticipationType,
-  type: UserParticipationType,
-  // should it even be possible to change date? maybe just cancel and become cook another day?)
-  args: {
-    // Using ID, to support different types of dinnerclubs in the future on the same day.
-    id: {
-      type: new NonNull(StringType),
-      description: 'ID of the dinnerclub to join'
+    //type: UserParticipationType,
+    type: UserParticipationType,
+    // should it even be possible to change date? maybe just cancel and become cook another day?)
+    args: {
+        // Using ID, to support different types of dinnerclubs in the future on the same day.
+        id: {
+            type: new NonNull(StringType),
+            description: 'ID of the dinnerclub to join'
+        },
+        participating: {
+            type: new InputObjectType({
+                name: 'Participating',
+                fields: attributeFields(Participation,{exclude: ['id','up_key','dp_key','createdAt','updatedAt']}),
+                description: 'The cooks own initial participation'
+            }),
+            description: 'Specific info about your participation like number of guests etc.'
+        }
     },
-    participating: {
-      type: new InputObjectType({
-        name: 'Participating',
-        fields: attributeFields(Participation,{exclude: ['id','up_key','dp_key','createdAt','updatedAt']}),
-        description: 'The cooks own initial participation'
-      }),
-      description: 'Specific info about your participation like number of guests etc.'
-    }
-  },
-  resolve: function(root, args, context, info) {
-    // This is how we must do csrf checks for now...
-    if(csrf_check(root)){
-      return Promise.reject(csrf_error_message);
-    }
+    resolve: function(root, args, context, info) {
+        // This is how we must do csrf checks for now...
+        if(csrf_check(root)){
+            return Promise.reject(csrf_error_message);
+        }
 
-    // Here we see how to test for the same day
-    return sequelize.transaction((t)=>
-      User.findById(root.request.user.id,{transaction: t}).then((user)=>{
-        // We try and find a record satisfying conditions that mean we are not allowed to insert or update
-        return DinnerClub.findOne({
-          include: [{model: Participation, as: 'participant', where: {up_key: user.id}}],
-          where: {
-            id: args.id
-          },
-          transaction: t
-        }).then((dinnerclub)=>{
-          console.log(dinnerclub.participant);
-          if(dinnerclub.shopping_complete){
-            Promise.reject('Shopping has completed, and it is no longer possible to change participation');
-          }
-          if(dinnerclub.participant){
-            // We update
-            const participationId = dinnerclub.participant[0].id;
-            return Participation.update(args.participating,{transaction: t, where: {id: participationId}})
-              .then((a,_)=>participationId);
-          } else {
-            // We insert
-            return Participation.create(args.participating,{transaction: t}).then((p)=>p);
-          }
-        })
-      })
-      // If integer, its the id of the element we want to return, if not it is the create object
-    ).then((res)=>Number.isInteger(res) ? resolver(Participation)(root, {id: res}, context, info) : res);
-  },
-  description: 'Changes or creates participation, based on if current user is participating'
+        // Here we see how to test for the same day
+        return sequelize.transaction((t)=>
+            User.findById(root.request.user.id,{transaction: t}).then((user)=>{
+                // We try and find a record satisfying conditions that mean we are not allowed to insert or update
+                return DinnerClub.findOne({
+                    include: [{model: Participation, as: 'participant', where: {up_key: user.id}}],
+                    where: {
+                        id: args.id
+                    },
+                    transaction: t
+                }).then((dinnerclub)=>{
+                    console.log(dinnerclub.participant);
+                    // TODO except will_be_late
+                    if(dinnerclub.shopping_complete &&
+                            // guest_count and cancelled can not be set if shopping is completed,
+                            // will accept 'will_be_late' though
+                        (!('guest_count' in args.participating) || !('cancelled' in args.participating))){
+                        Promise.reject('Shopping has completed, and it is no longer possible to change participation');
+                    }
+                    if(dinnerclub.participant){
+                        // We update
+                        const participationId = dinnerclub.participant[0].id;
+                        return Participation.update(args.participating,{transaction: t, where: {id: participationId}})
+                            .then((a,_)=>participationId);
+                    } else {
+                        // We insert
+                        return Participation.create(args.participating,{transaction: t}).then((p)=>p);
+                    }
+                })
+            })
+        // If integer, its the id of the element we want to return, if not it is the create object
+        ).then((res)=>Number.isInteger(res) ? resolver(Participation)(root, {id: res}, context, info) : res);
+    },
+    description: 'Changes or creates participation, based on if current user is participating'
 };
 
 export default participate;
