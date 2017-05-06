ab42946584e680494fc769f712c15cc4f2b8a731
Peter Alexander Garnæs <petergarnaes@gmail.com>
2017-05-06 16:43:37 +0200

    Fixed async loading of user settings. Fixed moving between async routes

--- app/async/components.js
+++ app/async/components.js
@@ -5,5 +5,5 @@ export default {
     // import('../components/login')
     login: (cb) => require.ensure([],() => cb(require('../components/login')),'login'),
     calendar: (cb) => require.ensure([],() => cb(require('../components/calendar/calendar_component')),'calendar'),
-    user_settings: (cb) => require.ensure([],cb(require('../components/user_settings')),'user_settings')
+    user_settings: (cb) => require.ensure([],() => cb(require('../components/user_settings')),'user_settings')
 }
--- app/components/async_component/index.js
+++ app/components/async_component/index.js
@@ -7,6 +7,20 @@ import { withComponentRegister } from '../../async/component_register_container'
 import { resolved_components } from '../../async/resolved_components.js';
 
 class AsyncComponent extends React.Component {
+    // Called on initial mount and every time this
+    // component is rendered with new props.
+    constructor(props){
+        super(props);
+        this.fetchRouteComponent = this.fetchRouteComponent.bind(this);
+        // Must reset, so route fetching succeeds
+        AsyncComponent.Component = null;
+        this.state = {
+            Component: AsyncComponent.Component
+        };
+        // Must fetch the bundle
+        this.fetchRouteComponent();
+    }
+
     static propTypes = {
         routeIdentifier: React.PropTypes.string.isRequired,
         retrieveComponent: React.PropTypes.func.isRequired,
@@ -14,14 +28,10 @@ class AsyncComponent extends React.Component {
     static Component = null;
     mounted = false;
 
-    state = {
-        Component: AsyncComponent.Component
-    };
-
-    componentWillMount() {
+    fetchRouteComponent(){
         // Checks if route was registered, ie. SSR loaded.
         if(!this.props.isRegistered(this.props.routeIdentifier)){
-            if ( this.state.Component === null ) {
+            if ( AsyncComponent.Component === null ) {
                 new Promise((resolve) => this.props.retrieveComponent(resolve)).then(m => m.default).then(Component => {
                     AsyncComponent.Component = Component;
                     console.log('Loading async, routeRegistered was: '+this.props.isRegistered(this.props.routeIdentifier));
@@ -37,6 +47,23 @@ class AsyncComponent extends React.Component {
             console.log('route was registered, should see an identical render');
             AsyncComponent.Component = resolved_components[this.props.routeIdentifier];
         }
+    }
+
+    // TODO: Probably removeable
+    componentWillReceiveProps(newProps) {
+        // This happens when we move between async components, in which case we
+        // must ensure we are requesting the bundle (cached or not).
+        // Apparently this is not called if we have a constructor...
+        if(!(newProps.routeIdentifier === this.props.routeIdentifier)){
+            console.log('Rendering new async route?');
+            AsyncComponent.Component = null;
+            this.fetchRouteComponent();
+        }
+    }
+
+    // Make sure to fetch on initial mount
+    componentWillMount() {
+        this.fetchRouteComponent();
     }
 
     componentDidMount() {
